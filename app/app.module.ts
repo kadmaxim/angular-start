ea6002ae1b57f0221e440b2c5a3109cf6cedbf88
Max <Max>
2016-12-05 17:40:41 +0200

    HTTP. Last Step.

--- app/app.module.ts
+++ app/app.module.ts
@@ -2,25 +2,37 @@ import {NgModule, Type}      from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 import {FormsModule}   from '@angular/forms';
 
+import { HttpModule }    from '@angular/http';
+
 import {AppComponent}   from './app.component';
 import {HeroDetailComponent} from './hero-detail.component';
+import {HeroSearchComponent} from './hero-search.component';
 import {HeroService} from "./hero.service";
 import {HeroesComponent} from "./heroes.component";
 import {DashboardComponent} from "./dashboard.component";
 import {AppRoutingModule} from "./app-routing.module";
+
+// Imports for loading & configuring the in-memory web api
+import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService }  from './in-memory-data.service';
+
+import './rxjs-extensions';
 
 
 @NgModule({
     imports: [
         BrowserModule,
         FormsModule,
+        HttpModule,
+        InMemoryWebApiModule.forRoot(InMemoryDataService),
         AppRoutingModule
     ],
     declarations: [
         AppComponent,
         HeroDetailComponent,
         HeroesComponent,
-        DashboardComponent
+        DashboardComponent,
+        HeroSearchComponent
     ],
     providers: [HeroService],
     bootstrap: [AppComponent]
--- app/hero-detail.component.ts
+++ app/hero-detail.component.ts
@@ -13,6 +13,15 @@ import 'rxjs/add/operator/switchMap';
     styleUrls: ['hero-detail.component.css']
 })
 export class HeroDetailComponent implements OnInit {
+    @Input()
+    hero: Hero;
+
+    constructor(
+        private heroService: HeroService,
+        private route: ActivatedRoute,
+        private location: Location
+    ) {}
+
     ngOnInit(): void {
         this.route.params
             .switchMap((params: Params) => this.heroService.getHero(+params['id']))
@@ -22,14 +31,10 @@ export class HeroDetailComponent implements OnInit {
     goBack(): void {
         this.location.back();
     }
+    save(): void {
+        this.heroService.update(this.hero)
+            .then(() => this.goBack());
+    }
 
-    @Input()
-    hero: Hero;
-
-    constructor(
-        private heroService: HeroService,
-        private route: ActivatedRoute,
-        private location: Location
-    ) {}
 
 }
